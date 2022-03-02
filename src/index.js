a04f8cbb3030bcb3ae1d4cdf563940f8957534b4
Yuleidy <yuleidy.gonzalez@prodigygame.com>
2022-03-02 15:29:22 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://apollo-lift-off-5.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
